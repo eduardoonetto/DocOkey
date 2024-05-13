9a6c567d4f7f30097bc82cbc8c34507ba97109b7
Eduardo Onetto <eduardo.onetto97@gmail.com>
2024-05-13 16:50:11 -0400

    Swagger Security Authorization

--- routes/documentos.py
+++ routes/documentos.py
@@ -1,16 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from models.documentos import Documentos
 from database.sqlite import add_documento, get_documentos_by_rut_and_institucion_id
+from fastapi.security import APIKeyHeader
 
 #Rutas de usuarios
 router = APIRouter()
+session_id = APIKeyHeader(name="Authorization")
 
 #Subir Documento a tabla documentos:
 @router.post('/documento/subir')
-async def upload_file(documento: Documentos):
-    print(documento.nombre_documento, documento.archivo_b64, documento.institucion_id, documento.fecha_creacion)
-    documento_id = add_documento(documento.nombre_documento, documento.archivo_b64, documento.institucion_id, documento.fecha_creacion, documento.signers)
-    return {documento.nombre_documento: documento.nombre_documento, "documento_id": documento_id}
+async def upload_file(documento: Documentos, Authorization: str = Depends(session_id)):
+    print(Authorization)
+    response = add_documento(documento.nombre_documento, documento.archivo_b64, documento.institucion_id, documento.fecha_creacion, documento.signers, Authorization)
+    return response
 
 #listar Documentos de un firmante segun su signer_rut e institucion_id:
 @router.get('/documento/listar/{signer_rut}/{signer_institucion}')
